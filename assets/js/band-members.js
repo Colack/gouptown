07558db3c06d08d44a5bf7329bb57f5ff52bd3b3
Colack <UniformKitten281@gmail.com>
2024-11-06 20:27:13 -0800

    It is 8:26pm and I can't tell if I'm falling asleep.

---
+++ assets/js/band-members.js
@@ -0,0 +1,114 @@
+const bandMembers = [
+    {
+        name: 'Jack',
+        instrument: 'Guitar, Drums, Keyboard, and the occasional Vocals',
+        description: `"I'm either the most talented or the most delusional member of the band." Nobody can tell what Jack is thinking, but he's always represents a distrust of authority and a love of music. Programmer, musician, and all-around good guy, Jack is the most likely to be found in a mental institution, but he's also the most likely to be found in the studio.`,
+        image: 'assets/images/jack.jpg',
+        regularSayings: [
+            'Ever heard of Linux?',
+            'Put me on aux.',
+            'Not gonna lie, [insert dumb statement]',
+            'WE LOVE MIN MIN'
+        ],
+        favoriteColor: 'purple',
+        favoriteMedia: 'Persona 5 / TriGun',
+        favoriteLetter: 'T',
+        favoriteNumber: 7
+    },
+    {
+        name: 'Phin',
+        instrument: 'Guitar and Vocals',
+        description: `Cursed with both deafness and blindness, most of us still wonder how Phin is able to play. God at Smash Bros, Phin is probably in the E-Sports room at the moment bullying kids at Smash Bros. A love for music, writing, and knowledge in general makes this man who he is. He's also extremely chill.`,
+        image: 'assets/images/phin.jpg',
+        regularSayings: [
+            'What did you say?',
+            'Crazy how we are on the same team!',
+            'blood_meridian_judge.jpg',
+            'Whats poppin?'
+        ],
+        favoriteColor: 'purple',
+        favoriteMedia: 'Omori',
+        favoriteLetter: 'J',
+        favoriteNumber: 13
+    },
+    {
+        name: 'Sergio',
+        instrument: 'Guitar, with Hopes and Dreams',
+        description: `The introvert of the group, Sergio is loved by all. He's found wherever no one else is, and he's always the one to bring the group back together. Literally the human embodiment of the Green Giant from those peas commercials, Sergio may be a giant, but he is gentle and kind.`,
+        image: 'assets/images/sergio.jpg',
+        regularSayings: [
+            'placeholder',
+            'placeholder',
+            'placeholder',
+            'placeholder'
+        ],
+        favoriteColor: 'placeholder',
+        favoriteMedia: 'placeholder',
+        favoriteLetter: 'placeholder',
+        favoriteNumber: 0
+    },
+    {
+        name: 'Lance',
+        instrument: 'Saxophone',
+        description: `Most likely Phin\'s lover, Lance is literally made of pure positive energy. An avergage monkey monday enjoyer, stock taker, and someone with bad manners in Smash Bros., Lance is a hoot to be around, and brings life to the band. When you see him in the hallway, always yell \'IS THAT LANCE? BEST PLAYER ON THE TEAM!?\'`,
+        image: 'assets/images/lance.jpg',
+        regularSayings: [
+            'placeholder',
+            'placeholder',
+            'placeholder',
+            'placeholder'
+        ],
+        favoriteColor: 'green',
+        favoriteMedia: 'Hello World - Go Uptown (This is one of our songs!)',
+        favoriteLetter: 'L',
+        favoriteNumber: 11
+    },
+    {
+        name: 'Steven',
+        instrument: 'Vocals',
+        description: 'placeholder',
+        image: 'assets/images/steven.jpg',
+        regularSayings: [
+            'placeholder',
+            'placeholder',
+            'placeholder',
+            'placeholder'
+        ],
+        favoriteColor: 'placeholder',
+        favoriteMedia: 'placeholder',
+        favoriteLetter: 'placeholder',
+        favoriteNumber: 0
+    },
+    {
+        name: 'Jared',
+        instrument: 'Vocals',
+        description: `Max cheeser 8000, Jared is certainly a personality. Best friends with Jack, Jared is either enjoying his life, or cracking jokes around him. Talented at cameraworks, filmmaking, and audio / visuals, Jared can be found on a Macbook editing his next masterpiece. It is unknown if Jared is a human or a robot that only knows how to spam Side B, but we love him all the same.`,
+        image: 'assets/images/jared.jpg',
+        regularSayings: [
+            'Just spot dodge it!',
+            'Skill issue',
+            'W / L',
+            'placeholder'
+        ],
+        favoriteColor: 'orange',
+        favoriteMedia: 'Guess Who - Okay Fine',
+        favoriteLetter: 'W',
+        favoriteNumber: 15
+    },
+    {
+        name: 'Aiden',
+        instrument: 'None',
+        description: 'Honorary member of Go Uptown, Aiden is little brother to everyone in the band. Aiden is either raging at whatever current game he is playing, or worrying about his friends that he secretly cares deeply about. Aiden is the most likely to be lost in the sauce.',
+        image: 'assets/images/aiden.jpg',
+        regularSayings: [
+            'placeholder',
+            'placeholder',
+            'placeholder',
+            'placeholder'
+        ],
+        favoriteColor: 'placeholder',
+        favoriteMedia: 'placeholder',
+        favoriteLetter: 'placeholder',
+        favoriteNumber: 0
+    }
+]
